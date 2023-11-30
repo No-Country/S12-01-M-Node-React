c2ad6586cf31800a6b3e30fd900f8adc6f6b8b61
Juan Martin De Rosa <juanmderosa@gmail.com>
2023-11-30 20:20:22 -0300

    Feat: Add HomeCategories

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,20 +1,24 @@
 import { CardHomePage } from "@/components/CardHomePage";
+import { HomeCategories } from "@/components/HomeCategories";
 
 export default function Home() {
   return (
-    <main className="px-10 py-[60px] min-h-[calc(100vh_-_80px)] flex justify-between">
-      <section className="w-[762px] h-[340px]">
-        <h1 className="text-6xl font-bold tracking-[2px] leading-[84.6px]">
-          Todos los eventos cercanos a vos para que hagas lo que más te
-          apasiona.
-        </h1>
-        <input
-          type="text"
-          placeholder="Buscar evento..."
-          className="mt-5 border border-[#D9D9D9] px-4 w-[458px] h-[60px] bg-[url('/svg/SearchIcon.svg')] bg-no-repeat bg-right rounded-lg font- font-medium text-sm text-[#666666] focus:outline-none  focus:border-[3px] focus:border-Principal "
-        />
+    <main className="min-h-[calc(100vh_-_80px)] ">
+      <section className="w-full flex justify-between px-10 py-[60px] ">
+        <article className="w-[762px] h-[340px]">
+          <h1 className="text-6xl font-bold tracking-[2px] leading-[84.6px]">
+            Todos los eventos cercanos a vos para que hagas lo que más te
+            apasiona.
+          </h1>
+          <input
+            type="text"
+            placeholder="Buscar evento..."
+            className="mt-5 border border-[#D9D9D9] px-4 w-[458px] h-[60px] bg-[url('/svg/SearchIcon.svg')] bg-no-repeat bg-right rounded-lg font- font-medium text-sm text-[#666666] focus:outline-none  focus:border-[3px] focus:border-Principal "
+          />
+        </article>
+        <CardHomePage />
       </section>
-      <CardHomePage />
+      <HomeCategories />
     </main>
   );
 }
