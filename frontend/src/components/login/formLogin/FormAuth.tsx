f91dc96dbb8a7d564538a4fe91fca420e983bb46
Fabricio Reyna <fabricioreynacordes@gmail.com>
2023-12-01 17:47:19 -0300

    LoginGoogleAuthArreglado

--- frontend/src/components/login/formLogin/FormAuth.tsx
+++ frontend/src/components/login/formLogin/FormAuth.tsx
@@ -1,32 +1,31 @@
 // Ensure you have the required imports
 "use client";
 import React from "react";
-/* import GoogleAuth from './google/GoogleAuth';
-import FacebookAuth from './facebook/facebookAuth'; */
+import GoogleAuth from './google/GoogleAuth';
 import AppleIcon from "@/assets/icons/Appleicon";
+import { FaFacebookF } from "react-icons/fa";
 
 type FormAuthProps = {};
 
 const FormAuth: React.FC<FormAuthProps> = (props: FormAuthProps) => {
-  //   const responseFacebook = (response) => {
-  //     console.log(response);
-  //   };
+
 
   return (
     <div className="flex flex-col justify-center items-center">
       <span>A través de</span>
       <ul className="flex justify-evenly">
-        <li className="p-2  border-2  shadow shadow-[#C6C6C9] rounded-full flex mx-5 my-4 items-center w-fit justify-center">
+        <li className="px-4 py-2  border-2  shadow shadow-[#C6C6C9] rounded-full flex mx-5 my-4 items-center w-fit cursor-pointer justify-center">
           <AppleIcon
             fill="fff"
-            className="w-8 flex justify-center items-center"
+            className="w-8 flex justify-center items-center "
           />
         </li>
-        <li className="p-2  border-2  shadow shadow-[#C6C6C9] rounded-full flex mx-5 my-4 items-center w-fit justify-center">
-          {/* <GoogleAuth/>    */}
+        <li className="py-2 px-3  border-2  shadow shadow-[#C6C6C9] rounded-full flex mx-5 my-4 items-center w-fit justify-center">
+           <GoogleAuth/>   
         </li>
-        <li className="p-2  border-2  shadow shadow-[#C6C6C9] rounded-full flex mx-5 my-4 items-center  justify-center w-[52px] h-[52px]">
-          {/* <FacebookAuth/>  */}
+        <li className="p-2  border-2  shadow shadow-[#C6C6C9] rounded-full flex mx-5 my-4 items-center cursor-pointer justify-center px-4">
+          <FaFacebookF
+            className=" flex justify-center items-center text-3xl text-blue-600  "/>
         </li>
       </ul>
     </div>
