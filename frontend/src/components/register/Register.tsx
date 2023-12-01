4e13b3ee350ed919b7435e2bd425a36190eafe8d
bagse <102260190+Bagse@users.noreply.github.com>
2023-12-01 18:43:13 -0500

    add register form

---
+++ frontend/src/components/register/Register.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { roboto } from "@/assets/font";
+import Link from "next/link";
+import RegisterForm from "./formRegister/FormRegister";
+import BoxImage from "./formRegister/BoxImage/BoxImage";
+
+type Props = {};
+
+const Register = (props: Props) => {
+  return (
+    <main className="flex items-center gap-20">
+      <BoxImage />
+      <section className="w-[500px] mx-10 my-10 space-y-4">
+        <div className="">
+          <h2 className={`text-[34px] leading-[51px] text-${roboto} font-bold`}>
+            Registrate Ahora
+          </h2>
+          <p
+            className={`text-base text-${roboto} text-[#666666] first-letter:uppercase`}
+          >
+            ingresa tus datos para crear una cuenta.
+          </p>
+        </div>
+        <RegisterForm />
+
+        <div className="my-4">
+          <p className="text-sm font-semibold">
+            Al registrarme acepto los{" "}
+            <span className="text-[#1e75f7] hover:underline font-medium">
+              Términos y condiciones
+            </span>
+          </p>
+        </div>
+        <div className="flex">
+          <Link href={"/login"} className="text-Principal underline text-base font-semibold">
+            Ya tengo cuenta en Eventry
+          </Link>
+        </div>
+      </section>
+    </main>
+  );
+};
+
+export default Register;
---
+++ frontend/src/components/register/formRegister/BoxImage/BoxImage.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import RegisterFormImge from "@/assets/img/RegisterFormImge.png";
+import Image from "next/image";
+import Link from "next/link";
+import { FaFacebookF, FaTwitter, FaYoutube } from "react-icons/fa";
+
+type Props = {};
+
+const BoxImage = (props: Props) => {
+  return (
+    <section className="relative">
+      <div>
+        <div className="relative">
+          <Image
+            src={RegisterFormImge}
+            alt="a"
+            width={1200}
+            height={500}
+            objectFit="cover"
+            className="w-[750px] h-[644px]"
+          />
+          <span className="absolute top-0 w-min-[300px] md:w-[750px] h-full bg-gradient-to-t from-[#BE316C] via-[#F006B0] to-[#F006B0] opacity-30"></span>
+        </div>
+      </div>
+      <div className="absolute top-[14rem] left-0 ml-10 flex flex-col justify-evenly w-20">
+        <Link
+          href={"#"}
+          className=" bg-white text-black my-4 w-12 h-12 rounded-full flex justify-center items-center"
+        >
+          <FaFacebookF className=" text-lg opacity-80" />
+        </Link>
+        <Link
+          href={"#"}
+          className=" bg-white text-black my-4 w-12 h-12 rounded-full flex justify-center items-center"
+        >
+          <FaYoutube className=" text-lg opacity-80" />
+        </Link>
+        <Link
+          href={"#"}
+          className=" bg-white text-black my-4 w-12 h-12 rounded-full flex justify-center items-center"
+        >
+          <FaTwitter className=" text-lg opacity-80" />
+        </Link>
+      </div>
+    </section>
+  );
+};
+
+export default BoxImage;
