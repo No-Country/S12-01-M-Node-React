888ddb0bb9adb3b690d36324224cbc52b0159ebd
Juan Martin De Rosa <juanmderosa@gmail.com>
2023-12-01 14:36:07 -0300

    dependency problems

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -15,36 +15,45 @@ export const NavBar = () => {
   return (
     <header className="h-20 flex items-center px-10 justify-between border border-b[1px] border-b-Principal">
       <nav className="gap-8 flex items-center">
-        <Link href="/" className="font-extrabold text-3xl">
+        <Link
+          href="/"
+          className="font-extrabold text-3xl">
           Eventry
         </Link>
-        { pathname == "/" ? (
+        {pathname == "/" ? (
           <>
- {links.map((link) => (
-  <Link
-    className={`text-lg font-semibold ${
-      link.path === pathname ? "text-Principal" : "text-black"
-    }`}
-    key={link.title}
-    href={link.path}
-  >
-    {link.title}
-  </Link>
-))}
+            {links.map((link) => (
+              <Link
+                className={`text-lg font-semibold ${
+                  link.path === pathname ? "text-Principal" : "text-black"
+                }`}
+                key={link.title}
+                href={link.path}>
+                {link.title}
+              </Link>
+            ))}
           </>
-        ) : ""}
-       
+        ) : (
+          ""
+        )}
       </nav>
       {pathname == "/" ? (
-         <aside className="flex items-center gap-2">
-         <Link href={"/login"} className="text-lg font-semibold">Iniciar Sesión</Link >
-         <p className="font-thin text-Grey">|</p>
-         <Link href={"/register"} className="text-lg font-semibold">Registrase</Link>
-       </aside>
+        <aside className="flex items-center gap-2">
+          <Link
+            href={"/login"}
+            className="text-lg font-semibold">
+            Iniciar Sesión
+          </Link>
+          <p className="font-thin text-Grey">|</p>
+          <Link
+            href={"/register"}
+            className="text-lg font-semibold">
+            Registrase
+          </Link>
+        </aside>
       ) : (
-       ""
+        ""
       )}
-
     </header>
   );
 };
