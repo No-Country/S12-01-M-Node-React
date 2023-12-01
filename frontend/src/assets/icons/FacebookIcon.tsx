2b5503c69399cba9b54417382cffb276ef4ce989
Juan Martin De Rosa <juanmderosa@gmail.com>
2023-12-01 15:04:58 -0300

    Dependencias uninstall

--- frontend/src/assets/icons/FacebookIcon.tsx
+++ frontend/src/assets/icons/FacebookIcon.tsx
@@ -1,10 +1,13 @@
-import React, { FC, HTMLProps } from 'react';
+import React, { FC, HTMLProps } from "react";
 
 interface FacebookIconProps extends HTMLProps<SVGSVGElement> {
   fill?: string;
 }
 
-const FacebookIcon: FC<FacebookIconProps> = ({ fill = '#1877f2', ...props }) => (
+const FacebookIcon: FC<FacebookIconProps> = ({
+  fill = "#1877f2",
+  ...props
+}) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     viewBox="0 0 24 24"
@@ -13,8 +16,7 @@ const FacebookIcon: FC<FacebookIconProps> = ({ fill = '#1877f2', ...props }) => 
     height="24px"
     width="24px"
     fill={fill}
-    {...props}
-  >
+    {...props}>
     <path d="M12 0C5.383 0 0 5.383 0 12s5.383 12 12 12 12-5.383 12-12S18.617 0 12 0zm2.57 12.007H13V20h-2v-7.993H9V8.93h2V7.293C11 5.153 12.32 4 14.286 4c1.214 0 2.144.143 2.714.214v2.36a2.94 2.94 0 0 0-1.93-.786c-2.214 0-2.786 1.679-2.786 2.857v1.72h2.782l-.445 2.077z" />
   </svg>
 );
