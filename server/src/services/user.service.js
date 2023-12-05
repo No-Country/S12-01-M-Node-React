a58819b7d525c48cf0f7d85cd1253e7fff8a952d
MartinBerdun <mberdun.98@gmail.com>
2023-12-05 16:32:55 -0300

    feat: Fix log in

--- server/src/services/user.service.js
+++ server/src/services/user.service.js
@@ -3,6 +3,7 @@ import {comparePassword} from "../helpers/encrypt.js";
 import AppError from "../helpers/appError.js";
 import { generateAndSignToken } from "../helpers/jwt.js";
 import jwt from "jsonwebtoken"
+import { UserDTO } from "../utils/user.dto.js";
 
 import config from "../config.js";
 const {COOKIE_NAME, JWT_SECRET} = config;
@@ -23,7 +24,9 @@ const loginUser = async  (req,res) => {
             throw new AppError('Invalid email or password', 401);
         }
 
-        const jwtUser = JSON.parse(JSON.stringify(user));
+        const userDto = new UserDTO(user);
+
+        const jwtUser = JSON.parse(JSON.stringify(userDto));
 
         const token = jwt.sign(jwtUser,JWT_SECRET, {expiresIn: "24h"})
 
@@ -33,7 +36,7 @@ const loginUser = async  (req,res) => {
     
         return res
         .cookie(COOKIE_NAME, token, {httpOnly: true})
-        .send({status:"success", message:"Logged in"})
+        .send({status:"success", message:"Logged in", payload : userDto})
 
     } catch (error) {
         console.log(error);
