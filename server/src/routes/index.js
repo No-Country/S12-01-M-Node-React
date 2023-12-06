8982e6adae108e24641deddbf8010d0c8e1c1ead
Juan Martin De Rosa <juanmderosa@gmail.com>
2023-12-06 18:03:26 -0300

    fix: No server

--- server/src/routes/index.js
+++
@@ -1,9 +0,0 @@
-import router from "../routes/auth.router.js";
-import authRouter from "../routes/auth.router.js";
-import { Router } from "express";
-
-const routerApi = Router();
-
-routerApi.use("/auth", authRouter);
-
-export default routerApi;
