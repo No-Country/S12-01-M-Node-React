f839f8373169d7eed3d39bbed58c5a204977ae45
Adrian <esteban.adrian.gonz@gmail.com>
2023-12-11 09:48:46 -0300

    feat: Add ticket routes

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,9 +1,11 @@
 import router from "../routes/auth.router.js";
 import authRouter from "../routes/auth.router.js";
+import ticketRouter from "../routes/ticket.router.js";
 import { Router } from "express";
 
 const routerApi = Router();
 
 routerApi.use("/auth", authRouter);
+routerApi.use("/tickets", ticketRouter);
 
 export default routerApi;
