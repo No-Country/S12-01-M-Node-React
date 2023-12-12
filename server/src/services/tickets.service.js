60baa5251b935ecfc51502e756b2008b29861869
Adrian <esteban.adrian.gonz@gmail.com>
2023-12-12 01:55:17 -0300

    feat: Refactor fn's

--- server/src/services/tickets.service.js
+++ server/src/services/tickets.service.js
@@ -3,33 +3,33 @@ import Event from "../models/event.model.js";
 import userModel from "../models/user.model.js";
 
 export const createTicketService = async (userid, event) => {
-try {
-    const eventModel = await Event.findOne({ name: event })
-    if (!eventModel) throw new Error(`Couldn't find event ${event}, please check the list of events to see a list of available events.`);
-    const ticket = await Ticket.create({
-        event_name: eventModel._id,
-        event_date: eventModel._id,
-    })
+    try {
+        const eventModel = await Event.findOne({ name: event })
+        if (!eventModel) throw new Error(`Couldn't find event ${event}, please check the list of events to see a list of available events.`);
+        const ticket = await Ticket.create({
+            event_name: eventModel.name,
+            event_date: eventModel.date_of_event,
+        })
 
-    const userUpdated = await userModel.updateOne({
-        _id: userid,
-    }, {
-        ticket: ticket._id
-    }, {
-        new: true,
-    })
+        const userUpdated = await userModel.updateOne({
+            _id: userid,
+        }, {
+            $addToSet: { tickets: ticket._id }
+        });
 
-    if (!userUpdated) throw new Error(`Couldn't update user ticket, please try again.`);
-    
-    return ticket;
-} catch (error) {
-    throw new Error(error.message);
-}
+        if (!userUpdated) throw new Error(`Couldn't update user ticket, please try again.`);
+
+        return ticket;
+    } catch (error) {
+        throw new Error(error.message);
+    }
 }
 
 export const getUser = async (userid) => {
     try {
-        return await userModel.findOne({ _id: userid }).populate('ticket', 'reference_number event_name event_date');
+        const user = await userModel.findOne({ _id: userid }).populate('tickets', 'reference_number event_name event_date');
+        if (!user) throw new Error(`User not found`);
+        return user;
     } catch (error) {
         throw new Error(error.message);
     }
@@ -37,15 +37,15 @@ export const getUser = async (userid) => {
 
 export const ticketAlreadyExists = async (userid, eventName) => {
     try {
-        const user = await userModel.findOne({ _id: userid }).populate('ticket', 'reference_number event_name event_date');
+        const user = await userModel.findOne({ _id: userid }).populate('tickets', 'reference_number event_name event_date');
         if (!user) throw new Error(`Couldn't find user`);
 
         const event = await Event.findOne({ name: eventName });
-        if (!event) throw new Error(`Couldn't find event ${event}, please check the list of events to see a list of available events.`);
+        if (!event) throw new Error(`Couldn't find event "${eventName}", please check the list of events to see a list of available events.`);
 
-        if (!user?.ticket) return false;
-        
-        return user?.ticket?.event_name.toString() === event?._id.toString();
+        if (!user?.tickets) return false;
+
+        return user?.tickets?.some(ticket => ticket.event_name === event?.name);
     } catch (error) {
         throw new Error(error.message);
     }
