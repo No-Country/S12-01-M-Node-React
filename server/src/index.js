fb2bddf46939cc7ccc8b7a9677af0ea2d84d80ba
Adrian <esteban.adrian.gonz@gmail.com>
2023-12-12 02:01:38 -0300

    feat: Add cors

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cookieParser from "cookie-parser"
 import passport from "passport";
+import cors from "cors";
 import config from "./config.js";
 import connectDB from "./database/db.js"
 import initializePassport from "./services/auth.service.js";
@@ -11,7 +12,10 @@ import router from "./routes/index.js";
 const {PORT} = config || 8080;
 
 const app = express();
-
+app.use(cors({
+    origin: '*',
+    credentials: true,
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended : true}));
 
