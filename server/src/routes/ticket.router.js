e9b18c48d51ddb9a115d0f29a83bb3be1212438a
Adrian <esteban.adrian.gonz@gmail.com>
2023-12-12 12:10:47 -0300

    feat: Add single ticket validator

--- server/src/routes/ticket.router.js
+++ server/src/routes/ticket.router.js
@@ -3,13 +3,14 @@ import passport from "passport";
 import { createTicketController, getController, getSingleTicketController } from "../controllers/tickets.controller.js";
 import { createTicketValidate } from "../helpers/validators/tickets/create.validator.js";
 import { getTicketValidate } from "../helpers/validators/tickets/get.validator.js";
+import { getSingleTicketValidate } from "../helpers/validators/tickets/single.validator.js";
 
 const router = Router();
 
 // Get single tocket by it's id and it's user id
 // params: userid  (ObjectId string)
 // query: ?reference=   (UUID string)
-router.get('/:userid', passport.authenticate('jwt', { session: false }), getSingleTicketController);
+router.get('/:userid', passport.authenticate('jwt', { session: false }), getSingleTicketValidate, getSingleTicketController);
 
 // Get all tickets by it's user id
 // params: userid  (ObjectId string)
