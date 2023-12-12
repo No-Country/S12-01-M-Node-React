57a08a8b4518b0777cf44f5bdca3457544cb78f6
Adrian <esteban.adrian.gonz@gmail.com>
2023-12-12 15:56:28 -0300

    fix: Change cors opts

--- server/src/index.js
+++ server/src/index.js
@@ -11,10 +11,17 @@ import router from './routes/index.js';
 const { PORT } = config || 8080;
 
 const app = express();
-app.use(cors({
-    origin: ['http://localhost:3000/', 'https://eventry-one.vercel.app/'],
-    credentials: true,
-}));
+app.use(cors());
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept'
+    );
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
