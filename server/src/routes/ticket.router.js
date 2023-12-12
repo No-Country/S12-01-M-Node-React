152551a7cda5fe96504eac14abb938e67dbab137
Adrian <esteban.adrian.gonz@gmail.com>
2023-12-12 01:53:06 -0300

    feat: Add get single ticket route

--- server/src/routes/ticket.router.js
+++ server/src/routes/ticket.router.js
@@ -1,21 +1,23 @@
 import { Router } from "express"
 import passport from "passport";
-import { createController, getController } from "../controllers/tickets.controller.js";
+import { createTicketController, getController, getSingleTicketController } from "../controllers/tickets.controller.js";
 import { createTicketValidate } from "../helpers/validators/tickets/create.validator.js";
 import { getTicketValidate } from "../helpers/validators/tickets/get.validator.js";
 
 const router = Router();
 
 // Get single tocket by it's id and it's user id
-// router.get('/:userid/:ticketid', passport.authenticate('jwt', { session: false }), createController);
+// params: userid  (ObjectId string)
+// query: ?reference=   (UUID string)
+router.get('/:userid', passport.authenticate('jwt', { session: false }), getSingleTicketController);
 
 // Get all tickets by it's user id
 // params: userid  (ObjectId string)
-router.get('/:userid/all', passport.authenticate('jwt', { session: false }), getTicketValidate, getController); 
+router.get('/:userid', passport.authenticate('jwt', { session: false }), getTicketValidate, getController); 
 
 // Create new ticket by user id
 // params: userid  (ObjectId string)
 // query: ?event=   (String)
-router.post('/:userid', passport.authenticate('jwt', { session: false }), createTicketValidate, createController);
+router.post('/:userid', passport.authenticate('jwt', { session: false }), createTicketValidate, createTicketController);
 
 export default router;
