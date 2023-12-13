c466105729c3d6df89dbd8d7e18ce7cb6968629f
Fabricio Reyna <fabricioreynacordes@gmail.com>
2023-12-13 10:41:40 -0300

    Help Dash Login

---
+++ frontend/src/components/AyudaComp/SectionQuestion.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+type Props = {}
+
+const SectionQuestion = (props: Props) => {
+  return (
+    <section className="border-2 border-red-500 w-full  pt-10  grid grid-rows-3  ">
+    <div className="ml-10">
+    <h3 className="text-Principal text-4xl font-bold ">Preguntas frecuentes</h3>
+    <div>
+
+    </div>
+
+    </div>
+</section>
+  )
+}
+
+export default SectionQuestion
