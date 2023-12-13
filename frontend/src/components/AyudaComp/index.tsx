0b8a5ffd1d34bb1ebf1db095eca27d05d50185b7
Fabricio Reyna <fabricioreynacordes@gmail.com>
2023-12-13 10:41:31 -0300

    Help Dash Login

--- frontend/src/components/AyudaComp/index.tsx
+++ frontend/src/components/AyudaComp/index.tsx
@@ -1,28 +1,69 @@
-import React from 'react'
+import React from "react";
+import { BiSearch } from "react-icons/bi";
+import "@/components/AyudaComp/style.css"
+import Link from "next/link";
+import SectionQuestion from "./SectionQuestion";
 
-type Props = {}
+type Props = {};
 
 const AyudaComp = (props: Props) => {
   return (
     <div>
-        <div className='bg-[url("https://res.cloudinary.com/dhb9rdaoc/image/upload/v1702439888/bztwwq1sxqx201xi04mv.jpg")] h-[333px] relative'>
-        <div className="bg-gradient-to-r from-pink-600 to-purple-800 h-full absolute top-0 left-0 w-full opacity-70 "></div>
-        <div className='relative bottom-0 h-full flex  justify-end p-10 flex-col'>
-        <h2 className='text-6xl font-bold text-[#fafafa] w-5/12'>¿Tenés preguntas?  Nosotros las respuestas</h2>
-
-
+      <div className='bg-[url("https://res.cloudinary.com/dhb9rdaoc/image/upload/v1702439888/bztwwq1sxqx201xi04mv.jpg")] h-[333px] relative'>
+        <div className="bg-gradient-to-r from-[#BE316C]  to-[#526FD666] h-full absolute top-0 left-0 w-full opacity-70 "></div>
+        <div className="relative bottom-0 h-full flex  justify-end p-10 flex-col">
+          <h2 className="text-6xl font-bold text-[#fafafa] w-5/12">
+            ¿Tenés preguntas? Nosotros las respuestas
+          </h2>
         </div>
+      </div>
+      <div className="  h-screen flex justify-evenly ">
+        <section className="border-2 border-black w-2/4 flex flex-col justify-start items-center pt-10">
+          <div className="bg-gradient-to-r   text-3xl from-rose-600 to-indigo-800  rounded-[10px] p-[1px]  ">
+
+            <div className="bg-white rounded-[10px] w-[350px] flex justify-evenly items-center">
+            <input
+              type="search"
+              name="a"
+              id=" a"
+              className="ml-2 border-2 border-transparent rounded-[10px] bg-transparent focus:xring-2 appearance-none text-lg ring-transparent p-2 focus:border-transparent outline-none"
+              placeholder="Buscar tema"
+          
+            />
+   
+            <BiSearch className="fill-Principal ml-6 " />
+
+            </div>
+
             
-            <section>
-                
-            </section>
-            <section>
-
-            </section>
-        </div>
-
+          </div>
+          <div className="  w-full flex justify-center items-center">
+          <nav className="mt-10  w-full flex  justify-center items-center ">
+                <ul className="h-fit w-1/2  flex flex-col justify-center items-center ">
+                    <li className="border-b-[#B3B3B3] border-b-[1px] w-full p-3">
+                        <Link href={"/"} className=" w-full cursor-pointer text-Principal">Preguntas frecuentes </Link>
+                    </li>
+                    <li className="border-b-[#B3B3B3] border-b-[1px] w-full p-3">
+                        <Link href={"/"} className=" w-full cursor-pointer text-[#4E4E4E] "> Sobre Eventry </Link>
+                    </li>
+                    <li className="border-b-[#B3B3B3] border-b-[1px] w-full p-3">
+                        <Link href={"/"} className=" w-full cursor-pointer text-[#4E4E4E]"> Términos y condiciones </Link>
+                    </li>
+                    <li className="border-b-[#B3B3B3] border-b-[1px] w-full p-3">
+                        <Link href={"/"} className=" w-full cursor-pointer text-[#4E4E4E]"> Políticas de privacidad </Link>
+                    </li>
+                    <li className="border-b-transparent border-b-[1px] w-full p-3">
+                        <Link href={"/"} className=" w-full cursor-pointer text-[#4E4E4E]"> Soporte </Link>
+                    </li>
+                                    </ul>
+            </nav>
+          </div>
+     
+        </section>
+        <SectionQuestion/>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default AyudaComp
+export default AyudaComp;
--- frontend/src/components/login/formLogin/FormLogin.tsx
+++ frontend/src/components/login/formLogin/FormLogin.tsx
@@ -1,83 +1,103 @@
-'use client'
-import { useState, useEffect } from "react";
-import { roboto } from "@/assets/font";
-import { LockClosedIcon } from "@heroicons/react/20/solid";
-import { EnvelopeIcon } from "@heroicons/react/24/outline";
-import React from "react";
-import { useForm, SubmitHandler } from "react-hook-form";
-
-import * as regexPatterns from "../../../helpers/Regex";
+'use client';
+import { useState, useEffect } from 'react';
+import { roboto } from '@/assets/font';
+import { LockClosedIcon, EnvelopeIcon } from '@heroicons/react/20/solid';
+import React from 'react';
+import { useForm, SubmitHandler } from 'react-hook-form';
+import * as regexPatterns from '../../../helpers/Regex';
+import { LOGIN_ROUTE, MAIN_ROUTE } from '@/routes';
+import { useRouter } from 'next/navigation';
 
 interface LoginFormInputs {
   gmail: string;
   password: string;
 }
 
+
 const LoginForm: React.FC = () => {
+  const router = useRouter();
   const {
-    register,
+        register,
     handleSubmit,
     formState: { errors },
+    reset,
   } = useForm<LoginFormInputs>();
 
-  // State to hold form data
-  const [formData, setFormData] = useState<LoginFormInputs | null>(null);
-
-  const onSubmit: SubmitHandler<LoginFormInputs> = (data) => {
-    setFormData(data);
-    console.log("Datos de inicio de sesión:", data);
-
+  const onSubmit: SubmitHandler<LoginFormInputs> = async (data) => {
+    try {
+      const res = await fetch(`${MAIN_ROUTE}${LOGIN_ROUTE}`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        credentials: 'same-origin',
+        body: JSON.stringify({
+          email: data.gmail,
+          password: data.password,
+        }),
+      });
+      if (res.ok) {
+        const responseData = await res.json();
+        console.log('Respuesta del servidor:', responseData);
+        reset();
+        router.push("/dash")
+        return responseData;
+      } else {
+        return Promise.reject({
+          err: true,
+          status: res.status,
+          statusText: res.statusText,
+        });
+      }
+    } catch (err) {
+      console.error('Respuesta erronea:', err);
+      return err;
+    }
   };
 
-  useEffect(() => {
-    console.log("FormData:", formData);
-  }, [formData]);
 
   return (
     <form
       onSubmit={handleSubmit(onSubmit)}
-      className={`flex flex-col text-${roboto}`}>
+      className={`flex flex-col text-${roboto}`}
+    >
       <div className="my-2">
-        <label
-          className="  text-[#4e4e4e]"
-          htmlFor="gmail">
-          {" "}
+        <label className="  text-[#4e4e4e]" htmlFor="gmail">
           Correo Electrónico
         </label>
 
-        <div className="flex items-center h-10 rounded-lg border-2 w-full px-5 py-6 border-[#D9D9D9] ">
+        <div className="flex items-center h-10 rounded-lg border-2 w-full px-5 py-6 border-[#D9D9D9]">
           <EnvelopeIcon className="text-base w-5 h-5 opacity-20" />
           <input
             type="email"
-            {...register("gmail", {
+            {...register('gmail', {
               required: true,
-                         })}
+            })}
             placeholder="Tu correo electrónico"
-            className="ring-transparent ring-0 w-full focus:ring-0 focus:ring-transparent text-[#4e4e4e] outline-none pl-2 appearance-none  "
+            className="ring-transparent ring-0 w-full focus:ring-0 focus:ring-transparent text-[#4e4e4e] outline-none pl-2 appearance-none"
             name="gmail"
             id="gmail"
           />
         </div>
-        {errors.gmail && <p>Este campo es requerido y debe ser un correo válido</p>}
+        {errors.gmail && (
+          <p>Este campo es requerido y debe ser un correo válido</p>
+        )}
       </div>
       <div className="my-4">
-        <label
-          className="  text-[#4e4e4e]"
-          htmlFor="password">
+        <label className="text-[#4e4e4e]" htmlFor="password">
           Contraseña
         </label>
 
-        <div className="flex items-center h-10 rounded-lg border-2 w-full px-5 py-6 border-[#D9D9D9] ">
+        <div className="flex items-center h-10 rounded-lg border-2 w-full px-5 py-6 border-[#D9D9D9]">
           <LockClosedIcon className="text-base w-5 h-5 opacity-20" />
 
           <input
             type="password"
-            {...register("password", {
+            {...register('password', {
               required: true,
-              
             })}
             placeholder="Tu contraseña"
-            className="ring-transparent ring-0 w-full focus:ring-0 focus:ring-transparent text-[#4e4e4e] outline-none pl-2 "
+            className="ring-transparent ring-0 w-full focus:ring-0 focus:ring-transparent text-[#4e4e4e] outline-none pl-2"
             name="password"
             id="password"
           />
@@ -87,12 +107,13 @@ const LoginForm: React.FC = () => {
         )}
 
         <span className="first-letter:uppercase text-[#1e75f7]   hover:underline-offset-3 hover:underline cursor-pointer py-2 underline">
-          Olvide mi contraseña
+          Olvidé mi contraseña
         </span>
       </div>
       <button
         type="submit"
-        className="py-[10px] px-6 bg-[#be316c] hover:bg-[#cf4f85] duration-300 rounded-lg font-medium text-xl text-white">
+        className="py-[10px] px-6 bg-[#be316c] hover:bg-[#cf4f85] duration-300 rounded-lg font-medium text-xl text-white"
+      >
         Iniciar Sesión
       </button>
     </form>
