8e0ff33854eaa56e30ad6634ed93e5194c7e8c71
Juan Martin De Rosa <juanmderosa@gmail.com>
2023-12-14 09:04:58 -0300

    Style: Register Page Correction

--- frontend/src/components/register/formRegister/BoxImage/BoxImage.tsx
+++ frontend/src/components/register/formRegister/BoxImage/BoxImage.tsx
@@ -6,36 +6,17 @@ import { FaFacebookF, FaTwitter, FaYoutube } from "react-icons/fa";
 
 const BoxImage = () => {
   return (
-    <section className="relative">
+    <section className="relative w-1/2">
       <div>
         <div className="relative">
           <Image
             src={RegisterFormImge}
             alt="a"
-            width={640}
-            height={832}
-            className=" object-contain"
+            className=" object-contain w-full min-h-full"
           />
-          <span className="absolute top-0 w-[640px] h-full bg-gradient-to-t from-[#BE316C] via-[#F006B0] to-[#F006B0] opacity-30"></span>
+          <span className="absolute top-0 w-full h-full bg-gradient-to-t from-[#BE316C] via-[#F006B0] to-[#F006B0] opacity-30"></span>
         </div>
       </div>
-      {/*       <div className="absolute top-[14rem] left-0 ml-10 flex flex-col justify-evenly w-20">
-        <Link
-          href={"#"}
-          className=" bg-white text-black my-4 w-12 h-12 rounded-full flex justify-center items-center">
-          <FaFacebookF className=" text-lg opacity-80" />
-        </Link>
-        <Link
-          href={"#"}
-          className=" bg-white text-black my-4 w-12 h-12 rounded-full flex justify-center items-center">
-          <FaYoutube className=" text-lg opacity-80" />
-        </Link>
-        <Link
-          href={"#"}
-          className=" bg-white text-black my-4 w-12 h-12 rounded-full flex justify-center items-center">
-          <FaTwitter className=" text-lg opacity-80" />
-        </Link>
-      </div> */}
     </section>
   );
 };
